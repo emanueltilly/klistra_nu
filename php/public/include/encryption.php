248c4db799fa0cb80bfa5b283856356fddad8fd4
Emanuel Tilly <emanuel@tillys.se>
2023-07-11 18:20:11 +0200

    Encryption changed to AES-256-GCM

--- php/public/include/encryption.php
+++ php/public/include/encryption.php
@@ -1,39 +1,41 @@
 <?php
 class Encryption
 {
-    private $cipher = "AES-256-CBC";
+    private $cipher = "AES-256-GCM";
     private $key;
-    private $iv;
 
     public function __construct($key)
     {
         $this->key = hash("sha256", $key, true);
-        $this->iv = random_bytes(16);
     }
 
     public function encrypt($data)
     {
+        $iv = random_bytes(16);
+        $tag = '';
         $encrypted = openssl_encrypt(
             $data,
             $this->cipher,
             $this->key,
-            0,
-            $this->iv
+            OPENSSL_RAW_DATA,
+            $iv,
+            $tag
         );
-        $encoded = base64_encode($encrypted . "::" . $this->iv);
+        $encoded = base64_encode($encrypted . "::" . $iv . "::" . $tag);
         return $encoded;
     }
 
     public function decrypt($data)
     {
         $decoded = base64_decode($data);
-        list($encrypted, $iv) = explode("::", $decoded, 2);
+        list($encrypted, $iv, $tag) = explode("::", $decoded, 3);
         $decrypted = openssl_decrypt(
             $encrypted,
             $this->cipher,
             $this->key,
-            0,
-            $iv
+            OPENSSL_RAW_DATA,
+            $iv,
+            $tag
         );
         return $decrypted;
     }
